f01e61f9bd52e81ba67829f25d9b3565a73240da
The Readyfy <thereadyfy@gmail.com>
2026-07-16 16:13:56 +0500

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Dubai-Power-Tools">
       <CartProvider>
         <WishlistProvider>
           <SearchProvider>
